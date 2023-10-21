0e186013dcc219fe3b4851f6a8c27ed2e11f7fe7
ar1june <arjun.aggarwal.cd.che19@itbhu.ac.in>
2023-10-21 20:51:49 +0530

    fixing typo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello wo rld again!");
+        System.out.println("Hello world again!");
     }
 }
